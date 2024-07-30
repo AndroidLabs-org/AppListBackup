4cc299537218becc8051f7329d86e336cd0f21ad
AndroidLabs-org <info@androidlabs.org>
2024-07-30 07:17:26 +0300

    chore: bump version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.androidlabs.applistbackup"
         minSdk = 33
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.1"
+        versionCode = 7
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
